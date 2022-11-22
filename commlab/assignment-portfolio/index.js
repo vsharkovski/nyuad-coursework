d57338e9114f78425495c68b25039f10dfca7bc0
Vladimir Sharkovski <vsarkovskii@gmail.com>
2022-11-22 16:57:14 +0400

    add two scrolling columns

--- commlab/assignment-portfolio/index.js
+++ commlab/assignment-portfolio/index.js
@@ -1,5 +1,16 @@
 const toggleNav = () => {
   document.body.dataset.nav =
     document.body.dataset.nav === "false" ? "true" : "false";
-  
 };
+
+/*
+Journeys
+Competitive Programming Repository
+Secret research-related project
+Commlab 30MMF
+Commlab Comic
+Commlab Sound
+Commlab Video
+
+
+*/
