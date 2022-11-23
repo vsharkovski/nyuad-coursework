0b058af3fff035a46160a166df1e1c5e692be9e4
Vladimir Sharkovski <vsarkovskii@gmail.com>
2022-11-23 12:24:30 +0400

    make stuff in column framework-like

--- commlab/assignment-portfolio/index.js
+++ commlab/assignment-portfolio/index.js
@@ -3,14 +3,45 @@ const toggleNav = () => {
     document.body.dataset.nav === "false" ? "true" : "false";
 };
 
-/*
-Journeys
-Competitive Programming Repository
-Secret research-related project
-Commlab 30MMF
-Commlab Comic
-Commlab Sound
-Commlab Video
+class ColumnData {
+  constructor(route, imageUrl) {
+    this.imageUrl = imageUrl;
+    this.route = route;
+  }
+}
 
+const interestsData = [
+  new ColumnData("#reading", "assets/images/pia23444-1600.jpg"),
+  new ColumnData("#photography", "assets/images/pia23444-1600.jpg"),
+  new ColumnData("#coding", "assets/images/pia23444-1600.jpg"),
+];
 
-*/
+const projectsData = [
+  new ColumnData("#journeys", "assets/images/pia23444-1600.jpg"),
+  new ColumnData("#cp-repo", "assets/images/pia23444-1600.jpg"),
+  new ColumnData("#commlab-30mmf", "assets/images/pia23444-1600.jpg"),
+  new ColumnData("#commlab-comic", "assets/images/pia23444-1600.jpg"),
+  new ColumnData("#commlab-sound", "assets/images/pia23444-1600.jpg"),
+  new ColumnData("#commlab-video", "assets/images/pia23444-1600.jpg"),
+];
+
+const addToColumn = (column, data) => {
+  data.forEach((info) => {
+    const div = document.createElement("div");
+    column.appendChild(div);
+
+    const a = document.createElement("a");
+    a.setAttribute("href", info.route);
+    div.appendChild(a);
+
+    const img = document.createElement("img");
+    img.setAttribute("src", info.imageUrl);
+    a.appendChild(img);
+  });
+};
+
+const leftColumn = document.querySelector(".left-column");
+const rightColumn = document.querySelector(".right-column");
+
+addToColumn(leftColumn, interestsData);
+addToColumn(rightColumn, projectsData);
