e9256b79ce486a85b3999fca12cfc7c7d904279b
Vladimir Sharkovski <vsarkovskii@gmail.com>
2023-03-02 21:39:28 +0400

    Update level-gen.py

--- introim/midterm/level-gen.py
+++ introim/midterm/level-gen.py
@@ -2,7 +2,7 @@ TX = 12
 TY = 12
 
 symbol_map = {
-    "tinv": "Tile None rx ry tx ty 1 1",
+    "tinv": "Tile None rx ry tx ty 1 0",
     "tbrk": "Tile Brick rx ry tx ty 1 1",
     "trtl": "Tile RockTL rx ry tx ty 1 1",
     "trtm": "Tile RockTM rx ry tx ty 1 1",
@@ -23,8 +23,8 @@ symbol_map = {
     "tblk": "Tile Black rx ry tx ty 0 0",
     "hrt0": "Tile Heart rx ry tx ty 0 0",
     "hrt1": "Tile Heart rx ry tx ty 1 0",
-    "ek01": ["NPC Knight rx ry tx ty 0 0 5 2", "Follow 1"],
-    "et01": ["NPC Tektite rx ry tx ty 0 0 8 1", "Patrol 2 3 2 7 7 7 7 2"],
+    "nk01": ["NPC Knight rx ry tx ty 0 0 5 2", "Follow 1"],
+    "nt01": ["NPC Tektite rx ry tx ty 0 0 8 1", "Patrol 2 3 2 7 7 7 7 2"],
     "p001": "Player rx ry tx ty 48 48 3 14"
 }
 
@@ -86,7 +86,7 @@ levels.append("""
     .... trbl trbm trbr .... .... .... .... .... .... .... ....
     .... .... .... .... .... .... .... .... .... .... .... ....
     .... .... .... .... .... .... .... .... .... .... .... ....
-    .... .... .... .... ek01 .... .... ek01 .... .... .... ....
+    .... .... .... .... nk01 .... .... nk01 .... .... .... ....
     tbrk tbrk tbrk tbrk tbrk .... .... tbrk tbrk tbrk tbrk tbrk
 
     0 1 #594c3f
@@ -97,7 +97,7 @@ levels.append("""
     tbrk .... .... .... .... .... .... .... .... .... .... tbrk
     tbrk .... .... .... .... .... .... .... .... .... .... tbrk
     tbrk .... .... .... .... .... .... .... .... .... .... tbrk
-    tbrk .... .... .... .... et01 .... .... .... .... .... tbrk
+    tbrk .... .... .... .... nt01 .... .... .... .... .... tbrk
     tbrk .... .... .... .... .... .... .... hrt0 .... .... tbrk
     tbrk .... .... .... .... .... .... .... .... .... .... tbrk
     tbrk .... .... .... .... .... .... .... .... .... .... tbrk
@@ -111,6 +111,8 @@ for levelIndex in range(len(levels)):
     ty = None
 
     with open(f"level{levelIndex + 1}.txt", "w") as file:
+        mapped_lines = [[], []]
+        
         for level_line in level.split("\n"):
             words = level_line.strip().split(" ")
             if len(words) <= 1:
@@ -137,11 +139,18 @@ for levelIndex in range(len(levels)):
                     continue
 
                 unmapped_lines = symbol_map[word]
+                index = 1
                 if type(unmapped_lines) == str:
-                   unmapped_lines = [unmapped_lines]
+                    unmapped_lines = [unmapped_lines]
+                    index = 0
                 
                 for unmapped in unmapped_lines:
                     mapped = unmapped.replace("rx", str(rx)).replace("ry", str(ry)).replace("tx", str(tx)).replace("ty", str(ty))
-                    file.write(f"{mapped}\n")
-
+                    mapped_lines[index].append(mapped)
+            
             ty += 1
+        
+        for lst in mapped_lines:
+            for line in lst:
+                file.write(f"{line}\n")
+            
