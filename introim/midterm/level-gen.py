5c0202f8edea982d02145d0c0d61c38cfc2f7dd3
Vladimir Sharkovski <vsarkovskii@gmail.com>
2023-03-02 20:08:59 +0400

    Update level-gen.py

--- introim/midterm/level-gen.py
+++ introim/midterm/level-gen.py
@@ -1,8 +1,25 @@
-TX = 10
-TY = 10
+TX = 12
+TY = 12
 
 symbol_map = {
-    "tb11": "Tile Brick rx ry tx ty 1 1",
+    "tbrk": "Tile Brick rx ry tx ty 1 1",
+    "trtl": "Tile RockTL rx ry tx ty 1 1",
+    "trtm": "Tile RockTM rx ry tx ty 1 1",
+    "trtr": "Tile RockTR rx ry tx ty 1 1",
+    "trbl": "Tile RockBL rx ry tx ty 1 1",
+    "trbm": "Tile RockBM rx ry tx ty 1 1",
+    "trbr": "Tile RockBR rx ry tx ty 1 1",
+    "twtl": "Tile WaterTL rx ry tx ty 1 0",
+    "twtm": "Tile WaterTM rx ry tx ty 1 0",
+    "twtr": "Tile WaterTR rx ry tx ty 1 0",
+    "twml": "Tile WaterML rx ry tx ty 1 0",
+    "twmm": "Tile WaterMM rx ry tx ty 1 0",
+    "twmr": "Tile WaterMR rx ry tx ty 1 0",
+    "twbl": "Tile WaterBL rx ry tx ty 1 0",
+    "twbm": "Tile WaterBM rx ry tx ty 1 0",
+    "twbr": "Tile WaterBR rx ry tx ty 1 0",
+    "tbsh": "Tile Bush rx ry tx ty 0 1",
+    "tblk": "Tile Black rx ry tx ty 0 0",
     "ek01": ["NPC Knight rx ry tx ty 0 0 5 2", "Follow 1"],
     "et01": ["NPC Tektite rx ry tx ty 0 0 8 1", "Patrol 2 3 2 7 7 7 7 2"],
     "p001": "Player rx ry tx ty 48 48 3 14"
@@ -14,28 +31,32 @@ levels.append("""
     DefaultBG #ffc17a
     
     0 0 None
-    .... .... .... .... .... .... .... .... .... ....
-    .... p001 .... .... .... .... .... .... .... ....
-    .... .... .... .... .... .... .... .... .... ....
-    .... .... .... .... .... .... .... .... .... ....
-    .... .... .... .... .... .... .... .... .... ....
-    .... .... .... .... .... .... .... .... .... ....
-    .... .... .... .... .... .... .... .... .... ....
-    .... .... .... .... .... .... .... .... .... ....
-    .... .... .... ek01 .... .... ek01 .... .... ....
-    tb11 tb11 tb11 tb11 .... .... tb11 tb11 tb11 tb11
+    .... .... .... .... .... .... .... .... .... .... .... ....
+    .... .... .... .... .... .... .... .... .... .... .... ....
+    .... p001 .... .... .... .... .... .... .... .... .... ....
+    .... .... .... .... tbsh .... .... twtl twtm twtr .... ....
+    .... .... .... tbsh .... .... .... twml twmm twmr .... ....
+    .... .... .... .... .... .... .... twbl twbm twbr .... ....
+    .... trtl trtm trtr .... .... .... .... .... .... .... ....
+    .... trbl trbm trbr .... .... .... .... .... .... .... ....
+    .... .... .... .... .... .... .... .... .... .... .... ....
+    .... .... .... .... .... .... .... .... .... .... .... ....
+    .... .... .... .... ek01 .... .... ek01 .... .... .... ....
+    tbrk tbrk tbrk tbrk tbrk .... .... tbrk tbrk tbrk tbrk tbrk
 
     0 1 #594c3f
-    tb11 .... .... .... .... .... .... .... .... tb11
-    tb11 .... .... .... .... .... .... .... .... tb11
-    tb11 .... .... .... .... .... .... .... .... tb11
-    tb11 .... .... .... .... .... .... .... .... tb11
-    tb11 .... .... .... .... .... .... .... .... tb11
-    tb11 .... .... .... .... .... .... .... .... tb11
-    tb11 .... .... .... .... .... .... .... .... tb11
-    tb11 .... .... .... .... et01 .... .... .... tb11
-    tb11 .... .... .... .... .... .... .... .... tb11
-    tb11 tb11 tb11 tb11 tb11 tb11 tb11 tb11 tb11 tb11
+    tbrk .... .... .... .... .... .... .... .... .... .... tbrk
+    tbrk .... .... .... .... .... .... .... .... .... .... tbrk
+    tbrk .... .... .... .... .... .... .... .... .... .... tbrk
+    tbrk .... .... .... .... .... .... .... .... .... .... tbrk
+    tbrk .... .... .... .... .... .... .... .... .... .... tbrk
+    tbrk .... .... .... .... .... .... .... .... .... .... tbrk
+    tbrk .... .... .... .... .... .... .... .... .... .... tbrk
+    tbrk .... .... .... .... et01 .... .... .... .... .... tbrk
+    tbrk .... .... .... .... .... .... .... .... .... .... tbrk
+    tbrk .... .... .... .... .... .... .... .... .... .... tbrk
+    tbrk .... .... .... .... .... .... .... .... .... .... tbrk
+    tbrk tbrk tbrk tbrk tbrk tbrk tbrk tbrk tbrk tbrk tbrk tbrk
 """)
 
 for levelIndex in range(len(levels)):
