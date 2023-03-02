2cee5eab0e4204fc7101e099e80471b6ea8d84bd
Vladimir Sharkovski <vsarkovskii@gmail.com>
2023-03-02 19:02:52 +0400

    Update level-gen.py

--- introim/midterm/level-gen.py
+++ introim/midterm/level-gen.py
@@ -3,15 +3,17 @@ TY = 10
 
 symbol_map = {
     "tb11": "Tile Brick rx ry tx ty 1 1",
-    "ek01": "NPC Knight rx ry tx ty 0 0 5 2\nFollow 1",
-    "et01": "NPC Tektite rx ry tx ty 0 0 8 1\nPatrol 2 3 2 7 7 7 7 2",
+    "ek01": ["NPC Knight rx ry tx ty 0 0 5 2", "Follow 1"],
+    "et01": ["NPC Tektite rx ry tx ty 0 0 8 1", "Patrol 2 3 2 7 7 7 7 2"],
     "p001": "Player rx ry tx ty 48 48 3 14"
 }
 
 levels = []
 
 levels.append("""
-    0 0
+    DefaultBG #ffc17a
+    
+    0 0 None
     .... .... .... .... .... .... .... .... .... ....
     .... p001 .... .... .... .... .... .... .... ....
     .... .... .... .... .... .... .... .... .... ....
@@ -23,7 +25,7 @@ levels.append("""
     .... .... .... ek01 .... .... ek01 .... .... ....
     tb11 tb11 tb11 tb11 .... .... tb11 tb11 tb11 tb11
 
-    0 1
+    0 1 #594c3f
     tb11 .... .... .... .... .... .... .... .... tb11
     tb11 .... .... .... .... .... .... .... .... tb11
     tb11 .... .... .... .... .... .... .... .... tb11
@@ -43,22 +45,36 @@ for levelIndex in range(len(levels)):
     ty = None
 
     with open(f"level{levelIndex + 1}.txt", "w") as file:
-        for line in level.split("\n"):
-            words = line.strip().split(" ")
-            if len(words) < 2:
+        for level_line in level.split("\n"):
+            words = level_line.strip().split(" ")
+            if len(words) <= 1:
                 continue
+                
             if len(words) == 2:
+                if words[0] == "DefaultBG":
+                    color = words[1]
+                    file.write(f"DefaultBG {color}\n")
+                   
+                continue
+                    
+            if len(words) == 3:
                 rx = int(words[0])
                 ry = int(words[1])
                 ty = 0
+                color = words[2];
+                file.write(f"Room {rx} {ry} {color}\n")
                 continue
 
             for tx in range(len(words)):
                 if words[tx] == "....":
                     continue
 
-                r = symbol_map[words[tx]]
-                r = r.replace("rx", str(rx)).replace("ry", str(ry)).replace("tx", str(tx)).replace("ty", str(ty))
-                file.write(f"{r}\n")
+                unmapped_lines = symbol_map[words[tx]]
+                if type(unmapped_lines) == str:
+                   unmapped_lines = [unmapped_lines]
+                
+                for unmapped in unmapped_lines:
+                    mapped = unmapped.replace("rx", str(rx)).replace("ry", str(ry)).replace("tx", str(tx)).replace("ty", str(ty))
+                    file.write(f"{mapped}\n")
 
             ty += 1
