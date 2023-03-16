16169d1aa7b0d3b68894c58e2290d7ac2f307095
Vladimir Sharkovski <vsarkovskii@gmail.com>
2023-03-16 17:20:30 +0400

    add map changing logic

--- soundscape/index.js
+++ soundscape/index.js
@@ -1,25 +1,70 @@
 import data from "./assets/data.json" assert { type: "json" };
 
-// Add "trigger" class to all DOM elements of all triggers.
+// Initialize global variables.
+let activeMap = null;
+let activeExperience = null;
+
+// Initialize triggers.
 for (let map of data.maps) {
   for (let trigger of map.triggers) {
-    const elem = document.getElementById(trigger.elementId);
-    elem.classList.add("trigger");
+    const element = document.getElementById(trigger.elementId);
+
+    // Add "trigger" class to the DOM element.
+    element.classList.add("trigger");
+
+    // Set up click event based on event type.
+    if (trigger.type == "changeMap") {
+      // Change map.
+      const newMap = data.maps.find((map) => map.name == trigger.target);
+      if (newMap === null) {
+        console.error("Could not find target map with name", trigger.target);
+      } else {
+        element.addEventListener("click", () => {
+          activeMap = newMap;
+          showMap(newMap);
+        });
+      }
+    } else if (trigger.type == "showExperience") {
+      // Change experience.
+      const newExperience = data.experiences.find(
+        (exp) => exp.name == trigger.target
+      );
+      if (newExperience === null) {
+        console.error(
+          "Could not find target experience with name",
+          trigger.target
+        );
+      } else {
+        element.addEventListener("click", () => {
+          activeExperience = newExperience;
+          showExperience(newExperience);
+        });
+      }
+    } else {
+      console.error("Trigger", trigger.name, "has unknown type", trigger.type);
+    }
   }
 }
 
-// Find initial active map and hide all other maps.
-let activeMap = data.maps.find((map) => map.name == "campus");
+// Find map to be initially active and show it.
+activeMap = data.maps.find((map) => map.name == "campus");
 if (activeMap === null) {
   console.error("Could not find initial active map.");
 } else {
+  showMap(activeMap);
+}
+
+// Function for showing a target map.
+function showMap(targetMap) {
   data.maps.forEach((map) => {
-    if (map != activeMap) {
-      const elem = document.getElementById(map.elementId);
-      elem.classList.add("hidden");
+    const element = document.getElementById(map.elementId);
+    if (map == targetMap) {
+      element.classList.remove("hidden");
+    } else {
+      element.classList.add("hidden");
     }
   });
 }
 
-// Active sound is initially null.
-let activeSound = null;
+// Function for showing an experience.
+function showExperience(experience) {}
