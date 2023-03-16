3ae9e46b93b3a01f6136ba36c28f987f3618ca9c
Vladimir Sharkovski <vsarkovskii@gmail.com>
2023-03-16 16:51:31 +0400

    add both svgs

--- soundscape/index.js
+++ soundscape/index.js
@@ -1,2 +1,25 @@
-import data from "./assets/data.json" assert { type: 'json' };
-console.log(data);
+import data from "./assets/data.json" assert { type: "json" };
+
+// Add "trigger" class to all DOM elements of all triggers.
+for (let map of data.maps) {
+  for (let trigger of map.triggers) {
+    const elem = document.getElementById(trigger.elementId);
+    elem.classList.add("trigger");
+  }
+}
+
+// Find initial active map and hide all other maps.
+let activeMap = data.maps.find((map) => map.name == "campus");
+if (activeMap === null) {
+  console.error("Could not find initial active map.");
+} else {
+  data.maps.forEach((map) => {
+    if (map != activeMap) {
+      const elem = document.getElementById(map.elementId);
+      elem.classList.add("hidden");
+    }
+  });
+}
+
+// Active sound is initially null.
+let activeSound = null;
